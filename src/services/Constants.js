618d8b8d362f59465bb736f2d90fcfe02765ed18
Yazidal <yazid.al3@gmail.com>
2022-12-09 14:46:28 +0100

    c

--- src/services/Constants.js
+++ src/services/Constants.js
@@ -1,3 +1,3 @@
-//  export const JSON_API = 'https://bleusky.herokuapp.com/api'
+export const JSON_API = 'https://bleusky.herokuapp.com/api'
 // export const JSON_API = 'https://localhost:7232/api'
- export const JSON_API = 'https://localhost:5000'
+//export const JSON_API = 'http://localhost:5000'
